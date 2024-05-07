2b873719dd7ff3926189e5d08bdce126e46b60df
AhmedEnnaime <97989224+AhmedEnnaime@users.noreply.github.com>
2024-05-07 14:04:55 +0100

    initializing spirng project

---
+++ src/main/java/com/progress/bloomberg/BloombergApplication.java
@@ -0,0 +1,13 @@
+package com.progress.bloomberg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloombergApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BloombergApplication.class, args);
+    }
+
+}
