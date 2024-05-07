f05a51b76f786d1b4c8dcff7b492539a000e2923
AhmedEnnaime <97989224+AhmedEnnaime@users.noreply.github.com>
2024-05-07 22:25:55 +0100

    added logs for exceptions

--- src/main/java/com/progress/bloomberg/config/GlobalHandlerException.java
+++ src/main/java/com/progress/bloomberg/config/GlobalHandlerException.java
@@ -1,6 +1,7 @@
 package com.progress.bloomberg.config;
 
 import com.progress.bloomberg.exceptions.IdAlreadyExistsException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -12,11 +13,13 @@ import java.util.HashMap;
 import java.util.Map;
 
 @RestControllerAdvice
+@Slf4j
 public class GlobalHandlerException {
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public Map<String, String> handleValidationExceptions(
             MethodArgumentNotValidException ex) {
+        log.error("Validation error occurred: {}", ex.getMessage());
         Map<String, String> errors = new HashMap<>();
         ex.getBindingResult().getAllErrors().forEach((error) -> {
             String fieldName = ((FieldError) error).getField();
@@ -29,6 +32,7 @@ public class GlobalHandlerException {
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(Exception.class)
     public Map<String, String> handleExceptions(Exception ex) {
+        log.error("An unexpected error occurred: {}", ex.getMessage(), ex);
         Map<String, String> errors = new HashMap<>();
         errors.put("error", ex.getMessage());
         return errors;
@@ -37,6 +41,7 @@ public class GlobalHandlerException {
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(IdAlreadyExistsException.class)
     public Map<String, String> handleIdAlreadyExistsException(IdAlreadyExistsException ex) {
+        log.error("ID already exists: {}", ex.getMessage());
         Map<String, String> errors = new HashMap<>();
         errors.put("error", ex.getMessage());
         return errors;
