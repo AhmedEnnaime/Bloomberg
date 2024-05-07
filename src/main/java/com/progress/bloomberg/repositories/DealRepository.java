1e7dffe765d60ba70e9729d1ac18e1d5410434f1
AhmedEnnaime <97989224+AhmedEnnaime@users.noreply.github.com>
2024-05-07 14:40:00 +0100

    created deal repository

---
+++ src/main/java/com/progress/bloomberg/repositories/DealRepository.java
@@ -0,0 +1,9 @@
+package com.progress.bloomberg.repositories;
+
+import com.progress.bloomberg.models.entities.Deal;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DealRepository extends JpaRepository<Deal, Long> {
+}
